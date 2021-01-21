d4fc9d7cba96512fa02fd256baf7d819fbed24cc
anor731 <76232195+anor731@users.noreply.github.com>
2021-01-21 21:51:36 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,123 @@
+drawGrid = (paper, grid, offset, points)=>{
+        let array = []
+        let i = 0
+        while(i<grid[0]) {
+                let arr = []
+                let j = 0
+                while(j<grid[1]) {
+                        var path = new paper.Path.Circle(new paper.Point(2*i*points+offset[0], 2*j*points+offset[1]), points - 7);
+                        path.style = {
+                            fillColor: new paper.Color(1, 0, 0),
+                            strokeColor: 'black',
+                            strokeWidth: 1
+                        };
+                        arr.push(path)
+                        j++
+                }
+                array.push(arr)
+                i++
+        }
+        return array
+}
+forFirst = (grid, el, call)=>{
+        let i = 0
+        while(i<el) {
+                call(grid[i%grid.length][0])
+                i++
+        }
+}
+window.addEventListener('resize', ()=>{
+        let canavas = document.getElementById("canvas")
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+})
+window.onload = ()=>{
+        let canavas = document.getElementById("canvas")
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+        let scope = new paper.PaperScope()
+        scope.setup(canvas)
+
+        let grid = drawGrid(paper, [47, 23], [160, 100], 16)
+        
+        forFirst(grid, 420, (el)=>{
+                el.style = {
+                        fillColor: "black",
+                }
+        })
+
+        setInterval(()=>{
+                grid.forEach((x)=>{
+                        x.forEach((y)=>{
+                                y.radius = 16 - 7
+                        })
+                })
+        },170)
+        
+        setInterval(()=>{
+                grid.forEach((x)=>{
+                        x.forEach((y)=>{
+                                y.radius = 16 - 4
+                        })
+                })
+        },420)
+        
+        setInterval(()=>{
+                grid.forEach((x)=>{
+                        x.forEach((y)=>{
+                                y.style.strokeWidth = '1px';
+                        })
+                })
+        }, 777)
+        
+        setInterval(()=>{
+                grid.forEach((x)=>{
+                        x.forEach((y)=>{
+                                y.style.strokeWidth = '2px';
+                        })
+                })
+        }, 1337)
+        
+        
+        setInterval(()=>{
+                grid.forEach((x)=>{
+                        x.forEach((y)=>{
+                                y.style.strokeColor = 'black';
+                        })
+                })
+        }, 666)
+        
+        setInterval(()=>{
+                grid.forEach((x)=>{
+                        x.forEach((y)=>{
+                                y.style.strokeColor = 'blue';
+                        })
+                })
+        }, 870)
+        
+        setInterval(()=>{
+                grid.forEach((x)=>{
+                        x.forEach((y)=>{
+                                y.style.fillColor = 'green';
+                        })
+                })
+        }, 999)        
+        
+        
+        setInterval(()=>{
+                grid.forEach((x)=>{
+                        x.forEach((y)=>{
+                                y.style.fillColor = 'red';
+                        })
+                })
+        }, 888)
+        
+        var text = new PointText(new Point(200, 50));
+                text.justification = 'center';
+                text.fillColor = 'black';
+                text.content = 'The contents of the point text';
+
+        console.log(grid)
+}
+
+
